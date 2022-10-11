8189d7a2020b09979e4dcd1f99490f732c986b66
Ninja Tec <contato.esdrassantos@gmail.com>
2022-10-11 17:25:44 -0300

    Update api.py

--- api_financas/api.py
+++ api_financas/api.py
@@ -1,22 +1,23 @@
 import psycopg2 as ps
-
+import pandas as pd
 
 class Api:
     
     parametros_db = {
         'host': 'localhost',
         'db_name': 'postgres',
-        'user': 'teste',
-        'password': '123',
+        'user': 'user_teste',
+        'password': '1234',
         'sslmode': 'disable'
     }
     
-    conexao_db = ps.extensions.connection
+    conexao_db = None
     
     def __init__(self) -> None:
         self.conexao_db = self.conectar()
     
     def conectar(self) -> ps.extensions.connection:
+        
         url = f"host={self.parametros_db['host']}\
             user={self.parametros_db['user']}\
             dbname={self.parametros_db['db_name']}\
@@ -28,9 +29,9 @@ class Api:
         self.conexao_db.close()
         
         
-    
-    
 if __name__ == '__main__':
-    print(Api().conectar().__class__)
-    
-    
+    usuarios = pd.read_sql_query(
+        sql='select * from financas.usuario',
+        con=Api().conexao_db
+        )
+    print(usuarios)
